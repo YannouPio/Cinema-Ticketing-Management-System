651016e9fe0d9678a7ba8a73c282aed11514143f
YannouPio <yannoupio@gmail.com>
2023-06-15 00:01:02 +0800

    Update: Signup but buggy

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,4 +1,13 @@
+import Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 createApp(App).mount('#app')
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
