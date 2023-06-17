050c3f6f49ab0ba4a23cc72224a2b5811a2745f6
YannouPio <yannoupio@gmail.com>
2023-06-17 23:26:20 +0800

    Update:Initialize database

--- management_system/management_system/settings.py
+++ management_system/management_system/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.anthtoken',
+    'rest_framework.authtoken',
     'corsheaders',
     'djoser',
 ]
