0a9ced6145c6b8760b11936f676a193f9390ff4d
YannouPio <yannoupio@gmail.com>
2023-06-18 17:59:08 +0800

    Update: add formatter

--- management_system_vue/src/App.vue
+++ management_system_vue/src/App.vue
@@ -1,35 +1,35 @@
 <template>
   <div>
-  <nav class = "navbar" role ="navigation" aria-label="main naviation" style="min-height: 5rem;">
-    <div class="navbar-brand">
-      <a class="navbar-item is-size-4" href="/">影院信息管理系统</a>
-    </div>
-  
-    <div id ="navbar-item" class="navbar-menu"> 
-      <div class ="navbar-start">
-        <a class ="navbar-item">主页</a>
-        <a class ="navbar-item">关于</a>
+    <nav class="navbar" role="navigation" aria-label="main naviation" style="min-height: 5rem;">
+      <div class="navbar-brand">
+        <a class="navbar-item is-size-4" href="/">影院信息管理系统</a>
       </div>
 
-      <div class="navbar-end">
+      <div id="navbar-item" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">主页</a>
+          <a class="navbar-item">关于</a>
+        </div>
 
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary"><strong>注册</strong></a>
-            <a class="button is-light">登陆</a>
+        <div class="navbar-end">
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary"><strong>注册</strong></a>
+              <a class="button is-light">登陆</a>
+            </div>
           </div>
+
         </div>
 
       </div>
 
-    </div>
+    </nav>
+    <router-view />
 
-  </nav>
-  <router-view/>
-
-  <footer class="'footer'">
-    <p class="has-text-centered">3210421119彭靖强 (c) 2023</p>
-  </footer>
+    <footer class="'footer'">
+      <p class="has-text-centered">3210421119彭靖强 (c) 2023</p>
+    </footer>
   </div>
 </template>
 
