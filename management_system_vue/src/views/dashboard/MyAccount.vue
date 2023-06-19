357e2e85ac07b43a9110b8b5dbc2bb7aeb539b22
YannouPio <yannoupio@gmail.com>
2023-06-19 22:27:26 +0800

    make it possible to log out

--- management_system_vue/src/views/dashboard/MyAccount.vue
+++ management_system_vue/src/views/dashboard/MyAccount.vue
@@ -7,8 +7,27 @@
       </div>
   
       <section class="section">
-        
+        <button @click="logout()" class="button is-danger">退出账户</button>
       </section>
     </div>
   </template>
   
+  
+  <script>
+  import axios from 'axios'
+   export default {
+    methods: {
+        logout() {
+            console.log('logout')
+
+            axios.defaults.headers.common['Authorization'] = ""
+
+            localStorage.removeItem('token')
+
+            this.$store.commit('removeToken')
+
+            this.$router.push('/')
+        }
+    }
+   }
+</script>
