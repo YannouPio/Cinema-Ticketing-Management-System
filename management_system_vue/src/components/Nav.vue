333ba2a7a25b94d308f6d18d17c366a947b490ef
YannouPio <yannoupio@gmail.com>
2023-06-20 16:55:57 +0800

    Hide login/signup buttons in menu when authenticated

--- management_system_vue/src/components/Nav.vue
+++ management_system_vue/src/components/Nav.vue
@@ -1,30 +1,34 @@
 <template>
+  <nav class="navbar is-info" role="navigation" aria-label="main naviation" style="min-height: 5rem;">
+    <div class="navbar-brand">
+      <router-link class="navbar-item is-size-4" to="/">影院信息管理系统</router-link>
+    </div>
 
-<nav class="navbar is-info" role="navigation" aria-label="main naviation" style="min-height: 5rem;">
-      <div class="navbar-brand">
-        <router-link class="navbar-item is-size-4" to="/">影院信息管理系统</router-link>
+    <div id="navbar-item" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">主页</router-link>
+        <router-link to="/about" class="navbar-item">关于</router-link>
+        <router-link to="/movies" class="navbar-item">电影</router-link>
       </div>
 
-      <div id="navbar-item" class="navbar-menu">
-        <div class="navbar-start">
-          <router-link to="/" class="navbar-item">主页</router-link>
-          <router-link to="/about" class="navbar-item">关于</router-link>
-          <router-link to="/movies" class="navbar-item">电影</router-link>
-        </div>
+      <div class="navbar-end">
 
-        <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <template v-if="$store.state.user.isAuthenticated">
+              <router-link to="/dashboard/my-account" class="button is-info">我的账户</router-link>
 
-          <div class="navbar-item">
-            <div class="buttons">
+            </template>
+            <template v-else>
               <a href="/sign-up" class="button is-primary"><strong>注册</strong></a>
               <a href="/log-in" class="button is-light">登陆</a>
-            </div>
+            </template>
           </div>
-
         </div>
 
       </div>
 
-    </nav>
+    </div>
 
+  </nav>
 </template>
