e7a77fe70203f54535d5a4a0f091911840741650
YannouPio <yannoupio@gmail.com>
2023-06-21 13:50:35 +0800

    Add: todo5.txt

--- management_system/management_system/settings.py
+++ management_system/management_system/settings.py
@@ -29,6 +29,16 @@ ALLOWED_HOSTS = []
 
 WEBSITE_URL = 'http://127.0.0.1:8000'
 
+REST_FRAMEWORK = {
+    'DATETIME_FORMAT': '%d.%m.%Y %H:%M',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
 # Application definition
 
 INSTALLED_APPS = [
