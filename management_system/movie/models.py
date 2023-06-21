cb34fcab3c16dcf78bf81235107fb4f0722118b7
YannouPio <yannoupio@gmail.com>
2023-06-21 15:09:01 +0800

    Add thumbnails or image to movies

--- management_system/movie/models.py
+++ management_system/movie/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.conf import settings
 # 创建你的模型在这里
 
 # Category模型表示电影可以归属的不同类别。
@@ -47,5 +48,13 @@ class Movie(models.Model):
     # 创建电影的日期，当电影首次创建时自动设定
     created_at = models.DateField(auto_now_add=True)
 
+    image = models.ImageField(upload_to='uploads', blank=True, null=True)
+
     def __str__(self):
         return self.title
+    
+    def get_image(self):
+        if self.image:
+            return settings.WEBSITE_URL + self.image.url
+        else:
+            return 'http://bulma.io/images/placeholders/1280x960.png'
