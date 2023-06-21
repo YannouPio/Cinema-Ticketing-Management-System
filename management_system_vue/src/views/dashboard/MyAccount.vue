02aa8725c31ce34b4999c71713546388ec3111ec
YannouPio <yannoupio@gmail.com>
2023-06-21 13:27:50 +0800

    token in the backend when log out

--- management_system_vue/src/views/dashboard/MyAccount.vue
+++ management_system_vue/src/views/dashboard/MyAccount.vue
@@ -1,33 +1,41 @@
 <template>
-    <div class="about">
-      <div class="hero is-info">
-        <div class="hero-body has-text-centered">
-          <h1 class="title">我的账户</h1>
-        </div>
+  <div class="about">
+    <div class="hero is-info">
+      <div class="hero-body has-text-centered">
+        <h1 class="title">我的账户</h1>
       </div>
-  
-      <section class="section">
-        <button @click="logout()" class="button is-danger">退出账户</button>
-      </section>
     </div>
-  </template>
+
+    <section class="section">
+      <button @click="logout()" class="button is-danger">退出账户</button>
+    </section>
+  </div>
+</template>
   
   
-  <script>
-  import axios from 'axios'
-   export default {
-    methods: {
-        logout() {
-            console.log('logout')
+<script>
+import axios from 'axios'
+export default {
+  methods: {
+    async logout() {
+      console.log('logout')
 
-            axios.defaults.headers.common['Authorization'] = ""
+      await axios
+        .post('/api/v1/token/logout/')
+        .then(response => {
+          console.log('Logged out')
+        })
 
-            localStorage.removeItem('token')
 
-            this.$store.commit('removeToken')
 
-            this.$router.push('/')
-        }
+      axios.defaults.headers.common['Authorization'] = ""
+
+      localStorage.removeItem('token')
+
+      this.$store.commit('removeToken')
+
+      this.$router.push('/')
     }
-   }
+  }
+}
 </script>
