30d14345a714d667383270d3dc0e42b7cd143869
YannouPio <yannoupio@gmail.com>
2023-06-23 19:40:13 +0800

    Add some comments to Movies components

--- management_system_vue/src/components/MoviesItem.vue
+++ management_system_vue/src/components/MoviesItem.vue
@@ -1,32 +1,31 @@
 <template>
     <div class="card">
-        <div class="card-image">
-            <figure class="image is-4by3">
-                <img :src="movie.get_image" alt="Placeholder image">
-            </figure>
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <!-- 绑定电影的图片到 img 元素的 src 属性 -->
+          <img :src="movie.get_image" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <!-- 显示电影的标题 -->
+            <p class="is-size-5">{{ movie.title }}</p>
+          </div>
         </div>
-
-        <div class="card-content">
-            <div class="media">
-                <div class="media-content">
-                    <p class="is-size-5">{{ movie.title }}</p>
-                </div>
-            </div>
-
-            <div class="content">
-                <p>{{ movie.short_description }}</p>
-                <router-link :to="{ name: 'Movie', params: { slug: movie.slug } }">更多</router-link>
-            </div>
-
+        <div class="content">
+          <!-- 显示电影的简短描述 -->
+          <p>{{ movie.short_description }}</p>
+          <!-- 为 "更多" 链接绑定路由，当点击时将导航到对应电影的详细信息页 -->
+          <router-link :to="{ name: 'Movie', params: { slug: movie.slug } }">更多</router-link>
         </div>
+      </div>
     </div>
-</template>
-
-<script>
-
-
-export default {
-    props: ['movie']
-}
-
-</script>
+  </template>
+  
+  <script>
+  export default {
+    props: ['movie'] // 接收从父组件传来的 movie 对象
+  }
+  </script>
+  
--- management_system_vue/src/views/Movie.vue
+++ management_system_vue/src/views/Movie.vue
@@ -1,57 +1,55 @@
 <template>
+    <!-- 展示电影的标题和长描述 -->
     <div class="movies">
-        <div class="hero is-info">
-            <div class="hero-body has-text-centered">
-                <h1 class="title">{{ movie.title }}</h1>
-            </div>
+      <div class="hero is-info">
+        <div class="hero-body has-text-centered">
+          <h1 class="title">{{ movie.title }}</h1> <!-- 渲染电影标题 -->
         </div>
+      </div>
     </div>
-
     <section class="section">
-        <div class="container">
-            <div class="columns content">
-                <div class="column is-2">
-                    <h2>电影上映信息</h2>
-
-                    <ul>
-                        <li><a href="#">影厅</a></li>
-                        <li><a href="#">上映时间</a></li>
-                    </ul>
-
-                </div>
-                <div class="column is-10">
-                    {{ movie.long_description }}
-                </div>
-            </div>
+      <div class="container">
+        <div class="columns content">
+          <div class="column is-2">
+            <h2>电影上映信息</h2>
+            <ul>
+              <li><a href="#">影厅</a></li>
+              <li><a href="#">上映时间</a></li>
+            </ul>
+          </div>
+          <div class="column is-10">
+            {{ movie.long_description }} <!-- 渲染电影长描述 -->
+          </div>
         </div>
+      </div>
     </section>
-</template>
-
-
-<script>
-import axios from 'axios'
-
-export default {
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  export default {
     data() {
-        return {
-            movie: []
-        }
+      return {
+        movie: [] // 存储电影详细信息
+      }
     },
-
     mounted() {
-        console.log('mounted')
-
-        const slug = this.$route.params.slug
-
-        axios
-            .get(`movies/${slug}/`)
-            .then(response => {
-                console.log(response.data)
-
-                this.movie = response.data
-
-            })
+      console.log('mounted')
+  
+      // 从路由参数中获取电影的slug
+      const slug = this.$route.params.slug
+  
+      // 发送GET请求获取电影详细信息
+      axios
+        .get(`movies/${slug}/`)
+        .then(response => {
+          console.log(response.data)
+  
+          // 更新movie的值
+          this.movie = response.data
+        })
     }
-}
-
-</script>
+  }
+  </script>
+  
